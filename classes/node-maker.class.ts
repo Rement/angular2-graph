235d6cc3138bd24e489d5c9a0a994550bcbe0fcb
Andrei Kaliachka <akaliachka@gmail.com>
2018-05-22 10:59:26 +0300

    some style changes

--- classes/node-maker.class.ts
+++ classes/node-maker.class.ts
@@ -1,35 +1,37 @@
 import { TreeDiagramNode } from './node.class';
+
 export class TreeDiagramNodeMaker extends TreeDiagramNode {
     public isMaker = true;
+
     public drop(event) {
         event.preventDefault();
-        let guid = this.getThisNodeList().draggingNodeGuid;
+        const guid = this.getThisNodeList().draggingNodeGuid;
         this.getThisNodeList().rootNode(guid);
         this.displayName = 'New node';
         return false;
-  }
+    }
 
-    public dragenter (event) {
+    public dragenter(event) {
         event.dataTransfer.dropEffect = 'move';
-        let guid = this.getThisNodeList().draggingNodeGuid;
-        let node = this.getThisNodeList().getNode(guid);
+        const guid = this.getThisNodeList().draggingNodeGuid;
+        const node = this.getThisNodeList().getNode(guid);
         if (node.parentId) {
-          this.displayName = 'Root';
+            this.displayName = 'Root';
         }
-  }
+    }
 
-    public dragover (event) {
+    public dragover(event) {
         event.preventDefault();
-        let guid = this.getThisNodeList().draggingNodeGuid;
-        let node = this.getThisNodeList().getNode(guid);
+        const guid = this.getThisNodeList().draggingNodeGuid;
+        const node = this.getThisNodeList().getNode(guid);
         if (!this.isDragging && node.parentId) {
-          this.isDragover = true;
-          event.dataTransfer.dropEffect = 'move';
+            this.isDragover = true;
+            event.dataTransfer.dropEffect = 'move';
         }
         return false;
     }
 
-    public dragleave (event) {
+    public dragleave(event) {
         this.displayName = 'New node';
         this.isDragover = false;
     }
--- classes/node.class.ts
+++ classes/node.class.ts
@@ -1,4 +1,5 @@
 import { TreeDiagramNodesList } from './nodesList.class';
+
 export class TreeDiagramNode {
     public parentId: string | null;
     public guid: string;
@@ -11,92 +12,92 @@ export class TreeDiagramNode {
     public status: string;
     private _toggle: boolean;
 
-  constructor (props, config, public getThisNodeList: () => TreeDiagramNodesList) {
-    if (!props.guid) {
-      return;
+    constructor(props, config, public getThisNodeList: () => TreeDiagramNodesList) {
+        if (!props.guid) {
+            return;
+        }
+        for (const prop in props) {
+            if (props.hasOwnProperty(prop)) {
+                this[prop] = props[prop];
+            }
+        }
+
+        this._toggle = false;
+        this.status = props.status;
+
+        if (config.nodeWidth) {
+            this.width = config.nodeWidth;
+        }
+        if (config.nodeHeight) {
+            this.height = config.nodeHeight;
+        }
+        this.children = new Set(props.children as string[]);
     }
-    for (let prop in props) {
-      if (props.hasOwnProperty(prop)) {
-        this[prop] = props[prop];
-      }
+
+    public destroy() {
+        this.getThisNodeList().destroy(this.guid);
     }
 
-    this._toggle = false;
-    this.status = props.status;
-
-    if (config.nodeWidth) {
-      this.width = config.nodeWidth;
+    public get isExpanded() {
+        return this._toggle;
     }
-    if (config.nodeHeight) {
-      this.height = config.nodeHeight;
+
+    public hasChildren() {
+        return !!this.children.size;
     }
-    this.children = new Set(<string[]> props.children);
-  }
 
-  public destroy () {
-    this.getThisNodeList().destroy(this.guid);
-  }
-
-  public get isExpanded () {
-    return this._toggle;
-  }
-
-  public hasChildren () {
-    return !!this.children.size;
-  }
-
-  public toggle (state = !this._toggle) {
-    this._toggle = state;
-    return state && this.getThisNodeList().toggleSiblings(this.guid);
-  }
-
-  public childrenCount () {
-    return this.children.size;
-  }
-
-  public isRoot () {
-    return this.parentId == null;
-  }
-
-  public dragenter (event) {
-    event.dataTransfer.dropEffect = 'move';
-  }
-
-  public dragleave (event) {
-    this.isDragover = false;
-  }
-
-  public dragstart (event) {
-    event.dataTransfer.effectAllowed = 'move';
-    this.isDragging = true;
-    this.toggle(false);
-    this.getThisNodeList().draggingNodeGuid = this.guid;
-  }
-
-  public dragover (event) {
-    event.preventDefault();
-    if (!this.isDragging) {
-      this.isDragover = true;
+    public toggle(state = !this._toggle) {
+        this._toggle = state;
+        return state && this.getThisNodeList().toggleSiblings(this.guid);
     }
-    event.dataTransfer.dropEffect = 'move';
-    return false;
-  }
 
-  public dragend() {
-    this.isDragover = false;
-    this.isDragging = false;
-  }
+    public childrenCount() {
+        return this.children.size;
+    }
 
-  public drop (event) {
-    event.preventDefault();
-    let guid = this.getThisNodeList().draggingNodeGuid;
-    this.getThisNodeList().transfer(guid, this.guid);
-    return false;
-  }
+    public isRoot() {
+        return this.parentId == null;
+    }
 
-  public addChild() {
-    let newNodeGuid = this.getThisNodeList().newNode(this.guid);
-    this.children.add(newNodeGuid);
-    this.toggle(true);
-  }
+    public dragenter(event) {
+        event.dataTransfer.dropEffect = 'move';
+    }
+
+    public dragleave(event) {
+        this.isDragover = false;
+    }
+
+    public dragstart(event) {
+        event.dataTransfer.effectAllowed = 'move';
+        this.isDragging = true;
+        this.toggle(false);
+        this.getThisNodeList().draggingNodeGuid = this.guid;
+    }
+
+    public dragover(event) {
+        event.preventDefault();
+        if (!this.isDragging) {
+            this.isDragover = true;
+        }
+        event.dataTransfer.dropEffect = 'move';
+        return false;
+    }
+
+    public dragend() {
+        this.isDragover = false;
+        this.isDragging = false;
+    }
+
+    public drop(event) {
+        event.preventDefault();
+        const guid = this.getThisNodeList().draggingNodeGuid;
+        this.getThisNodeList().transfer(guid, this.guid);
+        return false;
+    }
+
+    public addChild() {
+        const newNodeGuid = this.getThisNodeList().newNode(this.guid);
+        this.children.add(newNodeGuid);
+        this.toggle(true);
+    }
 }
